51211fc7a2a7dd3d8146a76ef9176409cf0c03c1
Aditya <lapratomo24@yahoo.com>
2024-08-26 20:23:49 +0200

    added movements of the snake

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
--- snake.py
+++ snake.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 
 POSITIONS = [0, -20, -40]
-
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snake_block = []
         self.create_snake()
+        self.snake_head = self.snake_block[0]
 
     def create_snake(self):
         for position in range(0, 3):
@@ -22,4 +26,20 @@ class Snake:
             x_pos = self.snake_block[block - 1].xcor()
             y_pos = self.snake_block[block - 1].ycor()
             self.snake_block[block].goto(x_pos, y_pos)
-        self.snake_block[0].forward(20)
+        self.snake_head.forward(20)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
