9afa6f4c958c3c6e909b4e6cb3f31e6d64bdf71f
Aditya <lapratomo24@yahoo.com>
2024-09-03 16:16:28 +0200

    used a .txt file to retain highscore after exiting

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,12 @@
 from turtle import Turtle
-from food import Food
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -24,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.display_score()
 
